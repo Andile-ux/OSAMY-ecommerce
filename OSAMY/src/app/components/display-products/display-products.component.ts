7ffb66e965b38d15b9314aab595a2c64123ad504
Sunnyboy Chilwana <chilwanecorp@gmail.com>
2023-11-28 15:12:08 +0200

    displat-to-be-worked

--- OSAMY/src/app/components/display-products/display-products.component.ts
+++ OSAMY/src/app/components/display-products/display-products.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+
+import { StoreServiceService } from 'src/app/services/store-service.service';
 
 @Component({
   selector: 'app-display-products',
   templateUrl: './display-products.component.html',
   styleUrls: ['./display-products.component.css']
 })
-export class DisplayProductsComponent {
+export class DisplayProductsComponent implements OnInit{
 
+  constructor(private store:StoreServiceService){};
+
+  //create property to be called (same as array list)
+  pruductsData: any = [];
+
+  ngOnInit(): void {
+    //call method and test via console
+    this.store.getAllProducts().subscribe((allData)=>{
+      console.log(allData)
+      //assign to property
+      this.pruductsData = allData;
+    });
+  }
 }
