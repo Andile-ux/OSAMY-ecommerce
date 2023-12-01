3e0a0df50b37ab1ca978c8c0a6229eb7c2f045e9
MariaSovara <damacm1117@DAs-MacBook-Air.local>
2023-12-01 13:38:19 +0200

    Val

--- OSAMY/src/app/components/login/login.component.ts
+++ OSAMY/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -28,27 +28,29 @@ export class LoginComponent implements OnInit{
       mobile: ['']
     });
   }
-  login(){
-    this.http.get<any>("http://localhost:3000/users").subscribe((results)=>{
-      const user = results.find((a:any)=>{
-        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
-      });
-      if(this.registerForm==null){
-        alert("Registered successfully");
-      } else{
-        alert("Invalid")
-      } 
-      if(user){
-        alert("Login Success");
-        this.loginForm.reset();
-        this.router.navigate(['landing'])
-      }else{
-        alert("User not found");
-      }
-    },err=>{
-      alert("Something went wrong");
+  login() {
+    this.http.get<any>("http://localhost:3000/users").subscribe((results) => {
+        const user = results.find((a: any) => {
+
+          if(this.loginForm.value.email != '' && this.loginForm.value.password != ''){
+            return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password;
+          }
+          return false;
+            
+        });
+
+        if (user) {
+            alert("Login Success");
+            this.loginForm.reset();
+            this.router.navigate(['landing']);
+        } else {
+            alert("Invalid credentials");
+        }
+    }, err => {
+        alert("Something went wrong");
     });
-  }
+}
+
 
   register(){
       this.http.post<any>("http://localhost:3000/users", this.registerForm.value).subscribe((results)=>{
