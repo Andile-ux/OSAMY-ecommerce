91620c7a190907cb3b323b188dd458f7680c896a
andile-ux <jwayelephi01@gmail.com>
2023-12-04 11:21:24 +0200

    updated server

--- OSAMY/src/app/server.js
+++ OSAMY/src/app/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const jsonServer = require('json-server');
 const path = require('path');
@@ -8,15 +9,18 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-
+app.use(cors({
+  credentials: true,
+  origin: [' http://localhost:4200']
+}))
 const secretKey = '1234';
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   next();
+// });
 
 function authenticateToken(req, res, next) {
   const token = req.headers['authorization'];
@@ -43,9 +47,23 @@ function authenticateToken(req, res, next) {
 }
 
 
-const jsonServerRouter = jsonServer.router('.OSAMY\database\db.json');
-app.use('/api', authenticateToken, jsonServerRouter);
+app.post('users', (req, res) => {
+  const { email } = req.body;
 
+  const user = { email };
+
+  if (user) {
+    // Create a JWT token
+    const token = jwt.sign({ id: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
+    res.json({ token });
+    console.log(token)
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
+
+const jsonServerRouter = jsonServer.router(path.join(__dirname, '.OSAMY', 'database', 'db.json'));
+app.use('/api', authenticateToken, jsonServerRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
